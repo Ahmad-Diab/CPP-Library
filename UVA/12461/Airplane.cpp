37da44ef7ce3e13f89f2030d6fbbf08419578981
Ahmed Diab <a7mediab977@gmail.com>
2019-02-09 19:12:56 +0200

    add UVA_12461_Airplane

---
+++ UVA/12461/Airplane.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+int main()
+{
+    int x ;
+    
+    while(true)
+    {
+        cin >> x ; if(!x)break ;
+        cout << "1/2\n" ;
+    }
+
+   return 0 ;
+}
