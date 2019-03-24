8c901ce7049ad6da1772f7570d08f73a595c855b
Ahmed Diab <a7mediab977@gmail.com>
2019-03-24 21:57:09 +0200

    add Point and Vector

---
+++ Geometry/Vector.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+
+#define EPS 1e-9
+
+using namespace std ;
+
+template <typename  T> struct Vector ;
+
+template <typename T>
+struct Vector
+{
+    T x , y ;
+
+    Vector(T a , T b){x = a , y = b;}
+
+    Vector(Point<T> a, Point<T> b){Vector(b.x - a.x , b.y - a.x) ;}
+
+    Vector scale(double s){ return Vector(x * s , y * s) ;}
+
+    double dot(Vector v){ return x * v.x + y * v.y ; }
+
+    double cross (Vector v){ return x * v.y - y * v.x ;}
+
+    double norm2(){ return x * x + y * y ;}
+
+    Vector reverse(){ return Vector(-x , -y) ;}
+
+    Vector normalize()
+    {
+        double d = sqrt(norm2());
+        return scale(1 / d) ;
+    }
+
+};
+
+int main()
+{
+
+    return 0 ;
+}
