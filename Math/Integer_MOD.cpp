cc4873081471f2c45aa372bc50b1922ac59b5205
Ahmed Diab <a7mediab977@gmail.com>
2019-05-03 20:38:14 +0200

    modify _int MOD

--- Math/Integer_MOD.cpp
+++ Math/Integer_MOD.cpp
@@ -2,41 +2,41 @@
 
 using namespace std ;
 
-struct Integer{
+struct _int{
     int x ;
-    int MOD = 1e9 + 7 ;
-    Integer(){}
-    Integer(int x): x(x) {}
+    int MOD = int(1e9 + 7) ;
+    _int(){}
+    _int(int x): x(x) {}
     bool operator < (const int y) { return x < y ;}
-    bool operator < (const Integer y) { return x < y.x ;}
+    bool operator < (const _int y) { return x < y.x ;}
     bool operator > (const int y) { return x > y ;}
-    bool operator > (const Integer y) { return x > y.x ;}
+    bool operator > (const _int y) { return x > y.x ;}
     bool operator <= (const int y) { return x <= y ;}
-    bool operator <= (const Integer y) { return x <= y.x ;}
+    bool operator <= (const _int y) { return x <= y.x ;}
     bool operator >= (const int y) { return x >= y ;}
-    bool operator >= (const Integer y) { return x >= y.x ;}
+    bool operator >= (const _int y) { return x >= y.x ;}
     bool operator == (const int y) { return x == y ;}
-    bool operator == (const Integer y) { return x == y.x ;}
+    bool operator == (const _int y) { return x == y.x ;}
     bool operator != (const int y) { return x != y ;}
-    bool operator != (const Integer y) { return x != y.x ;}
+    bool operator != (const _int y) { return x != y.x ;}
     int operator + (const int y) { int ans = x + y ; if(ans >= MOD) return ans -= MOD ; return ans ;}
     int operator - (const int y) { int ans = x - y ; if(ans < 0) return ans += MOD ; return ans ;}
     int operator * (const int y) { return (1ll * x * y) % MOD ;}
-    int operator + (const Integer y) {int ans = this->x + y.x ; if(ans >= MOD) return ans -= MOD ; return ans ;}
-    int operator - (const Integer y) {int ans = this->x - y.x ; if(ans < 0) return ans += MOD ; return ans ;}
-    int operator * (const Integer y) { return (1ll * this->x * y.x) % MOD ;}
+    int operator + (const _int y) {int ans = this->x + y.x ; if(ans >= MOD) return ans -= MOD ; return ans ;}
+    int operator - (const _int y) {int ans = this->x - y.x ; if(ans < 0) return ans += MOD ; return ans ;}
+    int operator * (const _int y) { return (1ll * this->x * y.x) % MOD ;}
     int operator ~ (){ return ~x ;}
     int operator += (const int y){ return *this + y;}
     int operator *= (const int y){ return *this * y;}
     int operator -= (const int y){ return *this - y;}
-    int operator += (const Integer y){ return *this + y;}
-    int operator *= (const Integer y){ return *this * y;}
-    int operator -= (const Integer y){ return *this - y;}
-    int operator ++  (){ return (*this + Integer(1)) ;}
-    int operator --  (){ return *this - Integer(1) ;}
-    Integer powMod(int a , int e)
+    int operator += (const _int y){ return *this + y;}
+    int operator *= (const _int y){ return *this * y;}
+    int operator -= (const _int y){ return *this - y;}
+    int operator ++  (){ return (*this + _int(1)) ;}
+    int operator --  (){ return *this - _int(1) ;}
+    _int powMod(int a , int e)
     {
-        Integer ans = 1 ;
+        _int ans = 1 ;
         while(e > 0)
         {
             if(e & 1)
@@ -46,9 +46,9 @@ struct Integer{
         }
         return ans ;
     }
-    Integer powMod(Integer a , int e)
+    _int powMod(_int a , int e)
     {
-        Integer ans = 1 ;
+        _int ans = 1 ;
         while(e > 0)
         {
             if(e & 1)
@@ -59,19 +59,19 @@ struct Integer{
         return ans ;
     }
     int operator / (const int y){ return *this * powMod(y , MOD - 2) ;}
-    int operator / (const Integer y){ return *this * powMod(y , MOD - 2) ;}
+    int operator / (const _int y){ return *this * powMod(y , MOD - 2) ;}
     int operator /= (const int y){ return *this / y ;}
-    int operator /= (const Integer y){ return *this / y ;}
+    int operator /= (const _int y){ return *this / y ;}
     int operator % (const int y){ return x % y ;}
-    int operator % (const Integer y){ return x % y.x ;}
+    int operator % (const _int y){ return x % y.x ;}
     int operator %= (const int y){ return x % y ;}
-    int operator %= (const Integer y){ return x % y.x ;}
+    int operator %= (const _int y){ return x % y.x ;}
 };
-istream& operator>>(istream &in, Integer& i){
+istream& operator>>(istream &in, _int& i){
     in >> i.x ;
     return in;
 }
-ostream& operator<<(ostream &out, const Integer& i){
+ostream& operator<<(ostream &out, const _int& i){
     out << i.x ;
     return out;
 }
